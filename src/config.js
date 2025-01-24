bb06d4d15629f4520e31c457a34119f170fd5c69
Karl Brycz <kbrycz@umich.edu>
2025-01-24 17:40:53 -0600

    add bg

--- src/config.js
+++ src/config.js
@@ -9,3 +9,11 @@
 
   export const SERVER_BASE_URL =
   process.env.NEXT_PUBLIC_FLASK_URL || "https://api.nocabot.com";
+
+  // src/config.js
+
+// Add a separate remove-bg server if you want a different domain
+export const REMOVE_BG_SERVER_BASE_URL =
+process.env.NEXT_PUBLIC_REMOVE_BG_FLASK_URL || "http://localhost:5000";
+
+// In production, maybe "https://removebg.nocabot.com" but for local testing it's port 5000
